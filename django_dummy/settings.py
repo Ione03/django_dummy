56a9d53895062ff9008f5965d9381629fc96c9e4
Ione03 <suratiwan03@gmail.com>
2025-05-06 13:31:43 +0800

    Change environment

--- django_dummy/settings.py
+++ django_dummy/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
         'ENGINE'    : 'django.db.backends.mysql',
         'NAME'      : 'db_dummy',         
         'USER'      : 'root',
-        'PASSWORD'  : 'dbkominfo56227',
-        'HOST'      : 'http://192.168.13.26/',
+        'PASSWORD'  : '',
+        'HOST'      : '127.0.0.1',
         'PORT'      : 3306,
     }
 }
